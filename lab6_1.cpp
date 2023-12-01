af0fb95bea5c77245dbd7f7e73f099c9522ee62d
KarnPrasai <karn_prasai@cmu.ac.th>
2023-12-01 15:10:20 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x = 1;
+    int odd_num = 0;
+    int even_num = 0;
+    while(x != 0)
+    {  
+        cout << "Enter an integer: ";
+        cin >> x;
+        if(x != 0)
+        {
+            if(x % 2 == 0)
+            {
+                even_num += 1;
+            }
+            else
+            {
+                odd_num += 1;
+            }
+        }
+    }
+    cout << "#Even numbers = " << even_num << "\n";
+    cout << "#Odd numbers = " << odd_num;
     return 0;
 }
